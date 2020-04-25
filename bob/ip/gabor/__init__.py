ec2f257a99500431341a4743628a728dff6193f6
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-04-25 22:41:09 +0200

    Pickled bob.ip.gabor.Jet

--- bob/ip/gabor/__init__.py
+++ bob/ip/gabor/__init__.py
@@ -7,6 +7,7 @@ import bob.extension
 bob.extension.load_bob_library('bob.ip.gabor', __file__)
 
 from ._library import *
+from ._library import Jet as _Jet_C
 from . import version
 from .version import module as __version__
 from .version import api as __api_version__
@@ -20,3 +21,18 @@ def get_config():
 
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith('_')]
+
+
+class Jet(_Jet_C):
+    __doc__ = _Jet_C.__doc__
+
+    def __getstate__(self):
+        d = dict(self.__dict__)
+        d["jet"] = self.jet
+        d["length"] = self.length
+        return d
+
+    def __setstate__(self, d):
+        self.__dict__ = d
+        self.__init__(d["length"])
+        self.jet = d["jet"]        
